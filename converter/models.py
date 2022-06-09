44576f2f53d47572aa570310038188f651d5f609
pawanshukla9 <pawan.shuklaw9@gmail.com>
2022-06-09 08:55:14 -0400

    final changes

--- converter/models.py
+++ converter/models.py
@@ -1,5 +1,9 @@
 from django.db import models
+import os
 
 # Create your models here.
 class FilesUplaod(models.Model):
     file = models.FileField()
+
+    def filename(self):
+        return os.path.basename(self.file.name)
--- converter/views.py
+++ converter/views.py
@@ -1,24 +1,29 @@
 import os.path
+from os import walk
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import  FilesUplaod
-from FileConverter import  settings
+from .models import FilesUplaod
+from FileConverter import settings
+from django.core.files.base import ContentFile
 # Create your views here.
 def uploadFile(request):
     if request.method == "POST":
         uploadedFile = request.FILES["file"]
         document = FilesUplaod.objects.create(file=uploadedFile)
         document.save()
+        file_path = os.path.join(settings.MEDIA_ROOT)
+        os.system('audio-to-midi '+settings.MEDIA_ROOT+'/'+document.filename()+' -b 120 -t 30')
+        #os.system('rm '+settings.MEDIA_ROOT+'/'+document.filename())
+        os.system('mv ./'+document.filename()+'.mid '+file_path)
+        file_path = os.path.join(settings.MEDIA_ROOT)
+        with open(file_path +'/'+document.filename()+'.mid', 'rb') as fh:
+            content = ContentFile(fh.read(), name="./"+document.filename()+'.mid')
+            document = FilesUplaod.objects.create(file=content)
+            document.save()
+            fh.close()
         return redirect(downloadFile)
     return render(request, "fileUpload.html")
 
 def downloadFile(request):
     context = {'files': FilesUplaod.objects.all()}
     return render(request, 'download.html', context)
-    if request.method == "POST":
-        file_path = os.path.join(settings.MEDIA_ROOT)
-        with open(file_path, 'rb') as fh:
-            response = HttpResponse(fh.read(), content_type="application/file")
-            response['Content-Disposition'] = 'inline;filename=' + os.path.basename(file_path)
-            return response
-    return render(request, "download.html")
